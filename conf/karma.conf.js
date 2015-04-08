780e5319f5d0f20a377b366143792a98c1b47f27
zack Frazier <zfrazier@gmail.com>
2015-04-08 17:07:37 -0500

    adds bower

---
+++ conf/karma.conf.js
@@ -0,0 +1,17 @@
+module.exports = function(config){
+    config.set({
+    basePath : '../',
+    files : [
+      'src/bower/angular/angular.js',
+      'src/bower/angular-mocks/angular-mocks.js',
+      'src/js/**/*.js',
+      'test/unit/**/*.js'
+    ],
+    autoWatch: true,
+    frameworks: ['jasmine'],
+    browsers : ['Chrome', /*'Safari',*/ 'Firefox'],
+    plugins : ['karma-chrome-launcher', 
+               //'karma-safari-launcher', 
+               'karma-firefox-launcher', 
+               'karma-jasmine']
+})};
