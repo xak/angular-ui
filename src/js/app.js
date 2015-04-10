b0c9fa0cba404ed011a1946efac1af570fd37217
zack Frazier <zfrazier@gmail.com>
2015-04-10 17:51:58 -0500

    failed map attempt

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
 'use strict';
-
+function onGoogleReady() {
+    angular.bootstrap(document.getElementById("myApp"), ['myApp']);
+}
 /** app level module which depends on services and controllers */
 angular.module('myApp', ['myApp.controllers', 'myApp.directives', 'ui.utils']);
--- src/js/controllers.js
+++ src/js/controllers.js
@@ -34,5 +34,22 @@ angular.module('myApp.controllers', []).controller('helloWorldCtrl',function ($s
 			}
 			$timeout(function() { $scope.helpText = "" }, 10000);
 	};
+});	
 
-});
+
+angular.module('myApp.controllers', []).controller('MapCtrl',function ($scope, $timeout) {
+	$scope.myMarkers = [];
+
+	$scope.mapOptions = {
+			center: new google.maps.LatLng(37.782,-122.418),
+			zoom: 4,
+			mapTypeId: google.maps.MapTypeId.SATELLITE
+	};
+
+// 	var cloudLayer = new google.maps.weather.CloudLayer();
+// 
+// 	$timeout(function(){
+// 			cloudLayer.setMap($scope.myMap);
+// 	}, 1000);
+
+});	
