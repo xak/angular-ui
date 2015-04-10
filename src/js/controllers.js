64567c0e260dcc5c51d3d163a1001b4b3deaf3ac
zack Frazier <zfrazier@gmail.com>
2015-04-10 17:04:26 -0500

    adds jquery

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,6 +1,38 @@
 'use strict';
 
 /** Controllers */
-angular.module('myApp.controllers', []).controller('helloWorldCtrl',function ($scope) {
-        $scope.name = { first: "Jane", last: "Doe" };
-    });
+angular.module('myApp.controllers', []).controller('helloWorldCtrl',function ($scope, $timeout) {
+//	$scope.name = { first: "Jane", last: "Doe" };
+	$scope.person = { firstName: "Jane", lastName: "Doe" };
+	$scope.mask = "(999) 999-9999 ext 99";
+	$scope.getModel = function () {
+			return JSON.stringify($scope.person, undefined, 2);
+	};
+	
+	var colors = ["white", "yellow", "lightgray"];
+	var activeColor = 0;
+	$scope.modelStatus = function() {
+		return { backgroundColor: colors[activeColor] };
+	};
+	$scope.focusCallback = function() {
+		activeColor = 1;
+	};
+	$scope.blurCallback = function() {
+		activeColor = 2;
+		$timeout(function() { activeColor = 0; }, 2000);
+	};
+	$scope.helpKeyDown = function($event) {
+			console.log($event);
+			if ($event.target.getAttribute('ng-model') == 'person.firstName') {
+				$scope.helpText = "Enter your first name.";
+			}
+			if ($event.target.getAttribute('ng-model') == 'person.lastName') {
+				$scope.helpText = "Enter your last name.";
+			}
+			if ($event.target.getAttribute('ng-model') == 'person.phone') {
+				$scope.helpText = "Enter your phone number.";
+			}
+			$timeout(function() { $scope.helpText = "" }, 10000);
+	};
+
+});
--- test/protractor/spec.js
+++ test/protractor/spec.js
@@ -1,7 +1,7 @@
 describe('hello world app', function() {
   it('should have a title', function() {
     browser.get('http://localhost:8000/src/');
-    expect(browser.getTitle()).toEqual('Hello!');
+    expect(browser.getTitle()).toEqual('Angular UI Template');
   });
 });
 describe('name fields', function() {
